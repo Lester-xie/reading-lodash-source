3315c0dcc045aa599bde0efc47dea01d7e1e0db4
谢显东 <541898607@qq.com>
2018-01-31 19:24:25 +0800

    🍱 Add compact method

--- README.md
+++ README.md
@@ -1,9 +1,14 @@
-# reading-lodash
+# reading-lodash-source
 
 在早期学习JavaScript编码的时候，特别喜欢去找一些奇形怪状的数学公式用编程语言来实现。
 
 进入到工作中后，使用到了一些基础类库，lodash、underscore，一直没想起应该去看下源码，现在时间稍微有点充裕了，那就把odash提供的每一个方法都当做是一个问题来自己解决，然后再附上它的源码来学习。
 
-感谢这样优秀的类库给我们提供了便利，也开源了便于学习。
+感谢这样优秀的类库给我们提供了便利，同时开源了便于学习。
 
+# 感谢
 [lodash](https://lodash.com)
+
+# progress
+* [x] chunk
+* [x] compact
---
+++ src/views/compact.vue
@@ -0,0 +1,37 @@
+<template>
+  <post :methodName="`compact(array)`">
+    <template slot="explain">
+      创建一个新数组，包含原数组中所有的非假值元素。例如false, null, 0, "", undefined, 和 NaN 都是被认为是“假值”。
+    </template>
+    <template slot="selfCode">
+      function compact(array) {
+        if (Array.isArray(array)) {
+          return array.filter(i => i)
+        }
+      }
+
+      compact([1,2,3, NaN, undefined, 0, '', null, false, -1, {}, []])
+      // [ 1, 2, 3, -1, {}, [] ]
+    </template>
+    <template slot="lodashCode">
+      function compact(array) {
+        let resIndex = 0
+        const result = []
+
+        if (array == null) {
+          return result
+        }
+
+        for (const value of array) {
+          if (value) {
+            result[resIndex++] = value
+          }
+        }
+        return result
+      }
+    </template>
+    <template slot="lodashComment">
+      应该是出于兼容性的考虑，lodash 使用的方法都尽量避免使用API
+    </template>
+  </post>
+</template>
